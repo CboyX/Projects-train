2b4610b7c64d4d9cf24ed536679b441c19b5935f
ChenXiao <xiao.chen01@hand-china.com>
2019-01-09 15:46:59 +0800

    updata

--- springcloud-hand-example/springcloud-eureka-feign/src/main/java/com/hand/springcloudeurekafeign/service/ComputeClient.java
+++ springcloud-hand-example/springcloud-eureka-feign/src/main/java/com/hand/springcloudeurekafeign/service/ComputeClient.java
@@ -1,11 +1,11 @@
-package com.hand.service;
+package com.hand.springcloudeurekafeign.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("eurka-clientA")
+@FeignClient("eurka-client")
 public interface ComputeClient {
     @RequestMapping(method = RequestMethod.GET, value = "/add")
     Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
